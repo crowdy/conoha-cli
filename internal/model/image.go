1e4e3e6866911cef3cff24fd72be8ce51b4d560f
Tonghyun Kim <crowdy@gmail.com>
2026-03-27 10:54:28 +0900

    Expand Image model with disk_format, container_format, checksum, owner fields

--- internal/model/image.go
+++ internal/model/image.go
@@ -3,14 +3,18 @@ package model
 import "time"
 
 type Image struct {
-	ID         string    `json:"id" yaml:"id"`
-	Name       string    `json:"name" yaml:"name"`
-	Status     string    `json:"status" yaml:"status"`
-	MinDisk    int       `json:"min_disk" yaml:"min_disk"`
-	MinRAM     int       `json:"min_ram" yaml:"min_ram"`
-	Size       int64     `json:"size" yaml:"size"`
-	CreatedAt  time.Time `json:"created_at" yaml:"created_at"`
-	Visibility string    `json:"visibility" yaml:"visibility"`
+	ID              string    `json:"id" yaml:"id"`
+	Name            string    `json:"name" yaml:"name"`
+	Status          string    `json:"status" yaml:"status"`
+	DiskFormat      string    `json:"disk_format" yaml:"disk_format"`
+	ContainerFormat string    `json:"container_format" yaml:"container_format"`
+	MinDisk         int       `json:"min_disk" yaml:"min_disk"`
+	MinRAM          int       `json:"min_ram" yaml:"min_ram"`
+	Size            int64     `json:"size" yaml:"size"`
+	Checksum        string    `json:"checksum" yaml:"checksum"`
+	Visibility      string    `json:"visibility" yaml:"visibility"`
+	Owner           string    `json:"owner" yaml:"owner"`
+	CreatedAt       time.Time `json:"created_at" yaml:"created_at"`
 }
 
 type ImagesResponse struct {
@@ -20,4 +24,10 @@ type ImagesResponse struct {
 type ImageQuota struct {
 	ImageCount    int `json:"image_count" yaml:"image_count"`
 	ImageMaxCount int `json:"image_max_count" yaml:"image_max_count"`
+}
+
+type ImageCreateRequest struct {
+	Name            string `json:"name"`
+	DiskFormat      string `json:"disk_format"`
+	ContainerFormat string `json:"container_format,omitempty"`
 }
