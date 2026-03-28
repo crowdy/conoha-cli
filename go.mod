f035bdb8752ff17e4c6b380b6a883f35ed1795b7
Tonghyun Kim <crowdy@gmail.com>
2026-03-28 22:08:28 +0900

    Add SSH execution helpers: Connect, RunScript, RunCommand

--- go.mod
+++ go.mod
@@ -3,15 +3,16 @@ module github.com/crowdy/conoha-cli
 go 1.26.1
 
 require (
+	github.com/manifoldco/promptui v0.9.0
 	github.com/spf13/cobra v1.10.2
+	golang.org/x/crypto v0.49.0
+	golang.org/x/term v0.41.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
-	golang.org/x/sys v0.41.0 // indirect
-	golang.org/x/term v0.40.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
 )
---
+++ internal/ssh/exec_test.go
@@ -0,0 +1,45 @@
+package ssh
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestConnectMissingKey(t *testing.T) {
+	_, err := Connect(ConnectConfig{
+		Host:    "127.0.0.1",
+		Port:    "22",
+		User:    "root",
+		KeyPath: "/nonexistent/key",
+	})
+	if err == nil {
+		t.Fatal("expected error for missing key file")
+	}
+}
+
+func TestConnectDefaults(t *testing.T) {
+	_, err := Connect(ConnectConfig{
+		Host:    "192.0.2.1",
+		KeyPath: "/nonexistent/key",
+	})
+	if err == nil {
+		t.Fatal("expected error")
+	}
+	if !bytes.Contains([]byte(err.Error()), []byte("read key")) {
+		t.Errorf("expected 'read key' error, got: %v", err)
+	}
+}
+
+func TestRunScriptNilClient(t *testing.T) {
+	_, err := RunScript(nil, []byte("echo hi"), nil, &bytes.Buffer{}, &bytes.Buffer{})
+	if err == nil {
+		t.Fatal("expected error for nil client")
+	}
+}
+
+func TestRunCommandNilClient(t *testing.T) {
+	_, err := RunCommand(nil, "echo hi", &bytes.Buffer{}, &bytes.Buffer{})
+	if err == nil {
+		t.Fatal("expected error for nil client")
+	}
+}
